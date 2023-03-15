45f6e699565bf62c5b243c85a63c04b4c4ea535d
CasCodes <masterbytwo@gmail.com>
2023-03-15 14:32:23 +0100

    added ui dir

---
+++ search_ui/script.js
@@ -0,0 +1,9 @@
+
+function openOverlay() {
+    // to do: function chain that reads input or document & dislpays it in overlay
+    document.getElementById("overlay-container").style.width = "100%";
+}
+
+function closeOverlay() {
+    document.getElementById("overlay-container").style.width = "0%";
+}

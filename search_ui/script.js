0c2dcdb5c0f911b6058ecc63199b340f3450d789
cascodes <masterbytwo@gmail.com>
2023-03-17 15:52:03 +0100

    read single input

--- search_ui/script.js
+++ search_ui/script.js
@@ -1,7 +1,24 @@
+// reads the input claim(s)
+function readInput() {
+    //todo: check if file uploaded (and if txt, pdf file)
+    
+    //else read from bar
+    let text = document.getElementById('search-bar-input').value
+    // check if empty
+    if(text.length == 0) {
+        return 1
+    }
+
+    console.log(text)
+    return text
+}
 
 function openOverlay() {
     // to do: function chain that reads input or document & dislpays it in overlay
-    document.getElementById("overlay-container").style.width = "100%";
+    // read the input
+    readInput()
+    // open overlay
+    ocument.getElementById("overlay-container").style.width = "100%";
 }
 
 function closeOverlay() {
